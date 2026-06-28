3bdfc1f54e3d2badebb91a001ca88502e0a8c10f
SoyElJose19 <jo.espinosat@duocuc.cl>
2026-06-28 02:11:47 -0400

    ultima reestructuracion/agregados nuevos

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -1,9 +1,12 @@
 resource "aws_ecr_repository" "repo_front" {
-  name = "frontend-despacho"
+  name         = "frontend-despacho"
+  force_delete = true
 }
 resource "aws_ecr_repository" "repo_back_despachos" {
-  name = "backend-despachos"
+  name         = "backend-despachos"
+  force_delete = true
 }
 resource "aws_ecr_repository" "repo_back_ventas" {
-  name = "backend-ventas"
+  name         = "backend-ventas"
+  force_delete = true
 }
